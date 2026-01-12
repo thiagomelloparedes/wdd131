1ea8a8424a9de1f66b20f5d65f319dfaf169b90f
thiagomelloparedes <thiago.mello.paredes@gmail.com>
2026-01-11 23:13:44 -0500

    ya esta amigo

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// 1) Current year in the footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// 2) Last modified date/time in the footer
+document.getElementById("lastModified").innerHTML = document.lastModified;
