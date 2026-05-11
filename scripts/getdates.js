cb9f705fab66b3b755a0a3d01fef6499631a4ad1
thiagomelloparedes <thiago.mello.paredes@gmail.com>
2026-05-10 23:24:42 -0500

    si

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,4 +3,4 @@ const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
 
 document.getElementById("lastModified").textContent =
-    `Last Modified: ${document.lastModified}`;
+    `Last Modified: ${document.lastModified}`; 
